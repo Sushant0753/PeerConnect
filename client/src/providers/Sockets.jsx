e37d6c536187ab74f26580810753863cd61aabe4
Sushant Kumar <mail.sushantthakur@gmail.com>
2024-11-08 22:12:01 +0530

    test/Local

--- client/src/providers/Sockets.jsx
+++ client/src/providers/Sockets.jsx
@@ -1,23 +1,18 @@
-import React, { createContext, useContext, useMemo } from "react";
-import { io } from "socket.io-client";
+import React, {createContext, useContext, useMemo} from "react";
+import {io} from "socket.io-client";
 
 const SocketContext = createContext(null);
 
-export const useSocket = () => {
-  return useContext(SocketContext);
+export const useSocket = () =>{
+    return useContext(SocketContext);
 };
 
-export const SocketProvider = (props) => {
-  const socketURL =
-    process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_SOCKET_URL_PRODUCTION
-      : process.env.REACT_APP_SOCKET_URL;
 
-  const socket = useMemo(() => io(socketURL, { transports: ['websocket', 'polling'] }), [socketURL]);
-
-  return (
-    <SocketContext.Provider value={{ socket }}>
-      {props.children}
-    </SocketContext.Provider>
-  );
-};
+export const SocketProvider = (props) =>{
+    const socket = useMemo(()=> io("localhost:8000"), []);
+    return(
+        <SocketContext.Provider value={{socket}}>
+            {props.children}
+        </SocketContext.Provider>
+    )
+}
