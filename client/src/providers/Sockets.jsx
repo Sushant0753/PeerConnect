d9dccfaa50034f17bd0ff5bb88151b4ecbe01cad
Sushant Kumar <mail.sushantthakur@gmail.com>
2024-11-08 21:02:28 +0530

    test/Deploy2

--- client/src/providers/Sockets.jsx
+++ client/src/providers/Sockets.jsx
@@ -10,7 +10,7 @@ export const useSocket = () => {
 export const SocketProvider = (props) => {
   const socketURL =
     process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_SOCKET_URL_PROD
+      ? process.env.REACT_APP_SOCKET_URL_PRODUCTION
       : process.env.REACT_APP_SOCKET_URL;
 
   const socket = useMemo(() => io(socketURL), [socketURL]);
