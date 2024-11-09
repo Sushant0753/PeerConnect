b4d54492d75717b43461d3a9a4838107bb2a70cd
Sushant Kumar <mail.sushantthakur@gmail.com>
2024-11-09 20:03:30 +0530

    test/Deploy19

--- client/src/providers/Sockets.jsx
+++ client/src/providers/Sockets.jsx
@@ -1,31 +1,39 @@
-import React, {createContext, useContext, useMemo} from "react";
-import {io} from "socket.io-client";
+import React, { createContext, useContext, useMemo } from "react";
+import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
-export const useSocket = () =>{
+export const useSocket = () => {
     return useContext(SocketContext);
 };
 
-export const SocketProvider = (props) =>{
+export const SocketProvider = (props) => {
     const socketUrl = useMemo(() => {
-        if (process.env.NODE_ENV === 'production') {
-            console.log(process.env.REACT_APP_SOCKET_URL_PRODUCTION);
-            return process.env.REACT_APP_SOCKET_URL_PRODUCTION;
-            
-        } else {
-            console.log(process.env.REACT_APP_SOCKET_URL)
-            return process.env.REACT_APP_SOCKET_URL || 'http://localhost:8000';
-
+        if (import.meta.env.PROD) {
+            return import.meta.env.VITE_SOCKET_SERVER_URL;
         }
+        return 'http://localhost:8000';
     }, []);
 
-    const socket = useMemo(() => io(socketUrl), [socketUrl]);
-    console.log(socketUrl);
+    const socket = useMemo(() => 
+        io(socketUrl, {
+            withCredentials: true,
+            transports: ['websocket', 'polling'],
+            autoConnect: true
+        })
+    , [socketUrl]);
 
-    return(
-        <SocketContext.Provider value={{socket}}>
+    socket.on('connect_error', (err) => {
+        console.log('Connection error:', err);
+    });
+
+    socket.on('connect', () => {
+        console.log('Connected to socket server');
+    });
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
             {props.children}
         </SocketContext.Provider>
-    )
-}
+    );
+};
