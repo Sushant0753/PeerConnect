b2c93b3a0e301e168c9557681de346350c3bd577
Sushant Kumar <mail.sushantthakur@gmail.com>
2024-11-09 19:13:14 +0530

    test/Deploy15

--- client/src/providers/Sockets.jsx
+++ client/src/providers/Sockets.jsx
@@ -10,13 +10,18 @@ export const useSocket = () =>{
 export const SocketProvider = (props) =>{
     const socketUrl = useMemo(() => {
         if (process.env.NODE_ENV === 'production') {
+            console.log(process.env.REACT_APP_SOCKET_URL_PRODUCTION);
             return process.env.REACT_APP_SOCKET_URL_PRODUCTION;
+            
         } else {
+            console.log(process.env.REACT_APP_SOCKET_URL)
             return process.env.REACT_APP_SOCKET_URL || 'http://localhost:8000';
+
         }
     }, []);
 
     const socket = useMemo(() => io(socketUrl), [socketUrl]);
+    console.log(socketUrl);
 
     return(
         <SocketContext.Provider value={{socket}}>
