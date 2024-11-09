ace2f76c16931f4849ae8717970d3a2c3c21c18f
Sushant Kumar <mail.sushantthakur@gmail.com>
2024-11-09 19:15:51 +0530

    test/Deploy16

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
 
 const io = new Server(server, {
     cors: {
-        origin: ["/socket.io/"],
+        origin: ["https://peer-connect-1otf.vercel.app/socket.io/"],
         withCredentials : true
     }
 });
